4038401e3790c4cd6d4d94a68c9a003834e83261
Knibbaz <info@basabbink.nl>
2021-06-29 19:42:26 +0200

    Working Search Function Added

--- js/crypto.js
+++ js/crypto.js
@@ -5,6 +5,8 @@ axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=
                 <span>Market Cap</span> 
               </div>`;
 
+  console.log(result);
+
   result.data.forEach((crypto) => {
     html =
       html +
--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ function searcher(e) {
   let userInput = inputElement.value.trim();
   console.log(userInput);
 
-  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${userInput}&vs_currencies=usd`).then((result) => {
-    console.log(result.data[userInput].usd);
-  });
+  axios
+    .get(`https://api.coingecko.com/api/v3/simple/price?ids=${userInput}&vs_currencies=usd`)
+    .then((result) => {
+      document.getElementById("customCall").innerHTML = `<h1>${result.data[userInput].usd}</h1>`;
+    })
+    .catch(() => {
+      document.getElementById("customCall").innerHTML = `<h1>CRYPTO NOT FOUND</h1>`;
+    });
 }
