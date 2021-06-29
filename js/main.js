afc0b7b5aec70e3e01b5c71701a515d33b210d34
Knibbaz <info@basabbink.nl>
2021-06-29 19:26:24 +0200

    Get crypto information from API

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-let form = document.getElementById('my-data');
-form.addEventListener('submit', searcher)
+let form = document.getElementById("my-data");
+form.addEventListener("submit", searcher);
 
 function searcher(e) {
   e.preventDefault();
-  let inputElement = document.getElementById('search-for');
+  let inputElement = document.getElementById("search-for");
   let userInput = inputElement.value.trim();
-  console.log(userInput) 
+  console.log(userInput);
+
+  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${userInput}&vs_currencies=usd`).then((result) => {
+    console.log(result.data[userInput].usd);
+  });
 }
