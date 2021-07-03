e7eb325ed08fcb36dc6fc3dfe32f284932e80bdc
Cristian Campo <sr.grmy@gmail.com>
2021-07-03 14:32:37 -0500

    Added a different view for the data

--- js/crypto.js
+++ js/crypto.js
@@ -1,23 +1,29 @@
-axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false").then((result) => {
-  let html = `<div class="pair">
-                <span>Name</span>
-                <span>Current Price</span> 
-                <span>Market Cap</span> 
-              </div>`;
-
-  console.log(result);
-
-  result.data.forEach((crypto) => {
-    html =
-      html +
-        `<li>
-          <div class="pair">
-            <span>${crypto.name}</span>
-            <span>$ ${crypto.current_price}</span> 
-            <span>$ ${crypto.market_cap}</span> 
-          </div>
-        </li>`;
+callData();
+function callData() {
+  const HTTP_REQUEST = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false";
+  axios.get(HTTP_REQUEST).then((result) => {
+    
+    document.getElementById('display').innerHTML = '';
+    
+    result.data.forEach((crypto) => {
+      let searchFor = ['image', 'name', 'current_price', 'market_cap'];
+      
+      searchFor.forEach((item) => {
+        let div = document.createElement('div');
+        let content;
+        if (item == 'image') {
+          content = document.createElement('img');
+          content.setAttribute('src', crypto[item]);
+        } else {
+          content = document.createElement('span');
+          content.innerText = crypto[item];
+        }
+        div.appendChild(content);
+        document.getElementById('display').appendChild(div);
+      });
+      
+    });
+    console.info('Last local update on ', new Date());
   });
-
-  document.getElementById("cryptos").innerHTML = html;
-});
+  window.setTimeout(callData, 30000);
+}
